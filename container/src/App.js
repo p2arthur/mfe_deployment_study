9a3a3fe10beb55f57a826301e7d55adfb309c1bf
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-05-29 20:22:32 -0300

    change package.json

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import MarketingApp from './components/MarketingApp';
+import MarketingApp from './components/MarketingApp';
 
 export default function App() {
   return (
     <div>
       <h1>Container 12</h1>
       <hr />
-      {/* <MarketingApp /> */}
+      <MarketingApp />
     </div>
   );
 }
