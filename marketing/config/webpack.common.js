5ac8766488bea3002d24c952712c35741c06bf9c
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-05-29 19:28:26 -0300

    remake everything

---
+++ marketing/config/webpack.common.js
@@ -0,0 +1,20 @@
+const WebpackHtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new WebpackHtmlPlugin({ template: './public/index.html' })],
+};
